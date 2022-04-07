e46c138f046796306b7225b85cff23fdf30ce835
Sage <94207840+Sages0ft@users.noreply.github.com>
2022-04-07 22:36:20 +0200

    Seems to work now

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,10 @@ fn main() {
     env::set_var("JX_ACCESS_TOKEN", "123");
     env::set_var("JX_REFRESH_TOKEN", "456");
 
-    Command::new("awer");
+    Command::new("JagexLauncher")
+        .args(["oldschool", "../oldschool/oldschool.prm"])
+        .spawn()
+        .expect("failed spawning JagexLauncher");
+
+    loop {}
 }
