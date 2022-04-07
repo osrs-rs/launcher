e8c7dd255e1c853b69fe3ee11d8fddab3dcf294f
Sage <94207840+Sages0ft@users.noreply.github.com>
2022-04-07 22:31:36 +0200

    Set tokens

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::{env, process::Command};
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("JX_ACCESS_TOKEN", "123");
+    env::set_var("JX_REFRESH_TOKEN", "456");
+
+    Command::new("awer");
 }
