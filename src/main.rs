1f4630396d47d3df184d201c9618afdd3f656daf
Sage <94207840+Sages0ft@users.noreply.github.com>
2022-04-07 22:43:34 +0200

    Documented code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::{env, process::Command};
 
 fn main() {
+    // Set the access token (used for account related details)
     env::set_var("JX_ACCESS_TOKEN", "123");
+
+    // Set the refresh token (unused for the time being, but assumed for refreshing the token in the client)
     env::set_var("JX_REFRESH_TOKEN", "456");
 
+    // Launch the Oldschool Runescape client
     Command::new("JagexLauncher")
         .args(["oldschool", "../oldschool/oldschool.prm"])
         .spawn()
         .expect("failed spawning JagexLauncher");
-
-    loop {}
 }
